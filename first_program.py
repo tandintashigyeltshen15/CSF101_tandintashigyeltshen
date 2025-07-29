1369127038c917623c095365be40e3329160bb61
tandintashigyeltshen15 <gyeltshentandint439@gmail.com>
2025-07-29 16:45:29 +0600

    first commit

---
+++ first_program.py
@@ -0,0 +1,8 @@
+print("hello world")
+a=2
+b=3
+sum=(a+b)
+print("the sum of a and b is: ",sum)
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+print("you are",name,"and you are",age,"years old")
