de94e8b3f39b33e47f7118eac8a9e9ab71a4954b
tandintashigyeltshen15 <gyeltshentandint439@gmail.com>
2025-08-06 17:21:25 +0600

    commit

---
+++ csf.python exercise/basics/strings.py
@@ -0,0 +1,8 @@
+name="tandin tashi gyeltshen"
+print("name")
+greeting = "Hello, " + name + "!"
+print(greeting)
+greeting_f = f"Hello, {name}!"
+print(greeting_f)
+name_length = len(name)
+print(name_length)
