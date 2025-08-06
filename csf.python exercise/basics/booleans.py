4d7a3fedbf72126f212f495f9249000fe8ac30ed
tandintashigyeltshen15 <gyeltshentandint439@gmail.com>
2025-08-06 17:27:13 +0600

    commit

---
+++ csf.python exercise/basics/booleans.py
@@ -0,0 +1,7 @@
+is_student = True
+is_employed = False
+print(is_student, is_employed)
+is_student_and_employed = is_student and is_employed
+print(is_student_and_employed)
+is_student_or_employed = is_student or is_employed
+print(is_student_or_employed)
